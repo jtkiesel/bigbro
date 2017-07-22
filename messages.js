1175477078db9dae2f057f0ad2235425a06e1d3b
jtkiesel <rstudio@example.com>
2017-07-22 00:42:55 -0400

    Fixed leaderboard.

--- messages.js
+++ messages.js
@@ -2,6 +2,21 @@ const Discord = require('discord.js');
 
 const app = require('./app');
 
+const messageEnums = {
+	attachments: 'a',
+		filename: 'f',
+		url: 'u',
+	author: 'u',
+	channel: 'c',
+	deleted: 'd',
+	editedTimestamp: 't',
+	edits: 'e',
+		content: 'c',
+		timestamp: 't',
+	guild: 'g',
+	id: 'i'
+};
+
 const upsertMessageInDb = (message, deleted) => {
 	if (message.guild) {
 		const insert = formatMessage(message, deleted);
@@ -11,19 +26,19 @@ const upsertMessageInDb = (message, deleted) => {
 			{upsert: true}
 		).then(result => {
 			if (result.upsertedCount) {
-				//console.log(`insert to messages: ${JSON.stringify(insert)}`);
+				console.log(`insert to messages: ${JSON.stringify(insert)}`);
 			} else {
-				const edits = insert.edits;
+				const edits = insert.e;
 				const update = JSON.parse(JSON.stringify(insert));
-				delete update.edits;
+				delete update.e;
 				app.db.collection('messages').updateOne(
 					{_id: update._id,
-						$or: [{editedTimestamp: {$lt: update.editedTimestamp}},
-							{deleted: {$ne: update.deleted}, deleted: false}]},
-					{$set: update, $addToSet: {edits: {$each: edits}}}
+						$or: [{t: {$lt: update.t}},
+							{d: {$ne: update.d}, d: false}]},
+					{$set: update, $addToSet: {e: {$each: edits}}}
 				).then(result => {
 					if (result.modifiedCount) {
-						//console.log(`update to messages: ${JSON.stringify(insert)}`);
+						console.log(`update to messages: ${JSON.stringify(insert)}`);
 					}
 				}).catch(console.error);
 			}
@@ -32,30 +47,25 @@ const upsertMessageInDb = (message, deleted) => {
 };
 
 const formatMessage = (message, deleted) => {
-	const document = {
-		_id: `${message.channel.id}-${message.id}`,
-		attachments: Array.from(message.attachments.values()).map(attachment => {
-			const a = {
-				filename: attachment.filename,
-				url: attachment.url
-			};
-			if (attachment.proxyUrl) {
-				a.proxyUrl = attachment.proxyUrl;
-			}
-			return a;
-		}),
-		author: message.author.id,
-		channel: message.channel.id,
-		deleted: deleted
+	const document = {_id: `${message.channel.id}-${message.id}`};
+	if (message.attachments.size) {
+		document.a = Array.from(message.attachments.values()).map(attachment => ({
+			f: attachment.filename,
+			u: attachment.url || attachment.proxyUrl
+		}));
 	}
+	document.u = message.author.id;
+	document.c = message.channel.id;
+	document.d = deleted;
 	if (message.editedTimestamp) {
-		document.editedTimestamp = message.editedTimestamp;
+		document.t = message.editedTimestamp;
 	}
-	document.edits = message.edits.map(edit => ({content: edit.content, timestamp: edit.editedTimestamp || edit.createdTimestamp}));
-	if (message.guild) {
-		document.guild = message.guild.id;
-	}
-	document.id = message.id;
+	document.e = message.edits.map(edit => ({
+		c: edit.content,
+		t: edit.editedTimestamp || edit.createdTimestamp
+	}));
+	document.g = message.guild.id;
+	document.i = message.id;
 	return document;
 };
 
