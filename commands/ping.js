ae4e44024d207d08cb4f78f6d8af4b78efbf7292
jtkiesel <rstudio@example.com>
2017-07-24 04:47:48 -0400

    Small variable name change.

--- commands/ping.js
+++ commands/ping.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 const app = require('../app');
 
 module.exports = (message, args) => {
-	const time = Date.now();
+	const ping = Date.now();
 	const embed = new Discord.RichEmbed()
 		.setColor('RANDOM')
 		.setDescription('🏓 Pong!');
-	message.channel.send({embed}).then(msg => {
-		embed.setDescription(`${embed.description} \`${(Date.now() - time) / 1000}s\``);
-		msg.edit({embed})
+	message.channel.send({embed}).then(reply => {
+		embed.setDescription(`${embed.description} \`${(Date.now() - ping) / 1000}s\``);
+		reply.edit({embed})
 			.then(reply => app.addFooter(message, embed, reply))
 			.catch(console.error);
 	}).catch(console.error);
