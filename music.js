6f060949899aa6b7fc3708cf57ef516c019398e7
jtkiesel <rstudio@example.com>
2017-10-23 23:22:26 -0400

    Forgot to add music file.

---
+++ music.js
@@ -0,0 +1,19 @@
+const google = require('googleapis');
+
+const youtube = google.youtube({version: 'v3', auth: process.env.GOOGLE_KEY});
+
+const search = async (query, limit) => {
+	return new Promise((resolve, reject) => {
+		youtube.search.list({part: 'snippet', q: query, maxResults: limit}, (err, response) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response.items);
+			}
+		});
+	});
+};
+
+module.exports = {
+	search: search
+};
