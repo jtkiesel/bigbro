9a859f5d2b8fe0d98ccc15d57ccdd9cacb65456b
jtkiesel <jordantkiesel@gmail.com>
2017-11-10 17:26:01 -0500

    Went from embed back to simple message for welcome message.

--- app.js
+++ app.js
@@ -122,10 +122,7 @@ client.on('ready', async () => {
 client.on('error', console.error);
 
 client.on('guildMemberAdd', member => {
-	const embed = new Discord.RichEmbed()
-			.setColor("RED")
-			.setDescription(`Welcome, ${member}! To access this server, one of the <@&197816965899747328> must verify you.\nPlease take a moment to read our server <#197777408198180864>, then send a message here with your name (or username) and team ID (such as "Kayley, 24B" or "Jordan, BNS"), and/or ask one of the <@&197816965899747328> for help.`)
-	member.guild.systemChannel.send({embed});
+	member.guild.systemChannel.send(`Welcome, ${member}! To access this server, one of the <@&197816965899747328> must verify you.\nPlease take a moment to read our server <#197777408198180864>, then send a message here with your name (or username) and team ID (such as "Kayley, 24B" or "Jordan, BNS"), and/or ask one of the <@&197816965899747328> for help.`);
 });
 
 client.on('message', message => {
