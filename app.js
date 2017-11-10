93bd46d925a02a643d7fae0631dfd8f0c3967d0c
jtkiesel <jordantkiesel@gmail.com>
2017-11-09 20:16:58 -0500

    Fixed Admin mention by changing welcome message to an embed.

--- app.js
+++ app.js
@@ -122,7 +122,8 @@ client.on('ready', async () => {
 client.on('error', console.error);
 
 client.on('guildMemberAdd', member => {
-	member.guild.systemChannel.send(`Welcome, ${member}! To access this server, an <@378029083230994432> must verify you.\nPlease take a moment to read our server <#197777408198180864>, then send a message here with your name (or username) and team ID (such as "Kayley, 24B" or "Jordan, BNS"), and/or ask an <@378029083230994432> for help.`);
+	const embed = new Dicsord.RichEmbed().setDescription(`Welcome, ${member}! To access this server, an <@&378029083230994432> must verify you.\nPlease take a moment to read our server <#197777408198180864>, then send a message here with your name (or username) and team ID (such as "Kayley, 24B" or "Jordan, BNS"), and/or ask an <@&378029083230994432> for help.`)
+	member.guild.systemChannel.send({embed});
 });
 
 client.on('message', message => {
