697d8a6b6793fc513972b2c4dbde2e4f0a11dc67
jtkiesel <jordantkiesel@gmail.com>
2018-08-07 00:55:45 -0400

    Update package.json

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ let helpDescription = `\`${prefix}help\`: Provides information about all command
 let db, messages;
 
 const clean = text => {
-	if (typeof(text) === 'string') {
+	if (typeof text === 'string') {
 		return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203)).slice(0, 1990);
 	}
 	return text;
