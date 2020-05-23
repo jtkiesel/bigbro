e02497dcd635e8469403b644e822b3e0120e7538
Jordan Kiesel <jordantkiesel@gmail.com>
2020-05-22 20:18:54 -0400

    Fixes made during testing

--- src/commands/dq.ts
+++ src/commands/dq.ts
@@ -1,10 +1,19 @@
+import { Collection, Guild, GuildMember, Message, MessageEmbed, MessageMentions, Permissions, TextChannel } from 'discord.js';
 import moment from 'moment';
 import 'moment-timer';
+import { BulkWriteOperation } from 'mongodb';
 
-import { Collection, Guild, GuildMember, Message, MessageEmbed, TextChannel, User } from 'discord.js';
-import { Command, db } from '..';
+import { Command, db, userUrl } from '..';
 
-const durationRegex = /[0-9]+|(y|M|w|d|h|n|s|ms)/;
+export type Dq = {
+  _id: {
+    guild: string;
+    user: string;
+  };
+  dqEndTime: number;
+};
+
+const durationRegex = /^([0-9]+)\s*(y|M|w|d|h|m|s|ms)$/;
 
 const verifiedRoleId = '260524994646376449';
 const timeoutRoleId = '392860547626041345';
@@ -12,12 +21,12 @@ const timeoutChannelId = '392860089951846400';
 
 const doTimeout = async (
   guild: Guild,
-  dispatcher: {member: GuildMember; user: User},
+  dispatcher: GuildMember,
   members: Collection<string, GuildMember>,
   duration: moment.Duration,
   reason?: string): Promise<void> => {
   const channel = guild.channels.cache.get(timeoutChannelId) as TextChannel;
-	
+
   const membersStr = members.map(m => `${m}`).join('\n');
 
   // remove verified role and add DQ role for all mentioned members that are not already DQ'd
@@ -27,17 +36,14 @@ const doTimeout = async (
       member.roles.add(timeoutRoleId, 'DQ')
     ]));
 
-	
-  channel.send(`<@&${timeoutRoleId}>:`);
   const announcement = new MessageEmbed()
-    .setAuthor(dispatcher.member.displayName, dispatcher.user.displayAvatarURL(), `https://discordapp.com/users/${dispatcher.user.id}`)
+    .setAuthor(dispatcher.displayName, dispatcher.user.displayAvatarURL(), userUrl(dispatcher.user.id))
     .setTitle('Disqualification')
-    .setThumbnail('https://discordapp.com/assets/8e9a2d7ef44c1052a246e062e4c3f796.svg') // :checkered_flag:
+    .setThumbnail('https://ton.twimg.com/stickers/stickers/10855_raw.png') // :checkered_flag:
     .addField('Reason', reason || 'unspecified')
     .addField('Duration', duration.humanize())
     .addField('Affected users', membersStr);
-
-  channel.send(announcement);
+  await channel.send(announcement);
 };
 
 export const doUnTimeout = (member: GuildMember) => async (): Promise<void> => {
@@ -49,50 +55,59 @@ export const doUnTimeout = (member: GuildMember) => async (): Promise<void> => {
 
 class DqCommand implements Command {
   /**
-	  * format: <prefix> dq \S <duration> <mentions> ( \S <reason> )?
-	  *  where:
-	  *		<duration>: [0-9]+ <unit>
-	  *		    <unit>: ( y | M | w | d | h | m | s | ms )
-	  *		<mentions>: <mention> ( \S <mention> )*
-	  *		  <reason>: .+
-	  */
+   * format: <prefix> dq \S <duration> <mentions> ( \S <reason> )?
+   *  where:
+   *    <duration>: [0-9]+ <unit>
+   *        <unit>: ( y | M | w | d | h | m | s | ms )
+   *    <mentions>: <mention> ( \S <mention> )*
+   *      <reason>: .+
+   */
   async execute(message: Message, args: string): Promise<Message> {
     if (!message.guild) {
       return message.reply('that command is only available in servers.');
     }
-    if (!message.member.hasPermission('ADMINISTRATOR')) {
-      return message.reply('you must be an administrator to use that command');
+    if (!message.member.hasPermission(Permissions.FLAGS.MANAGE_ROLES)) {
+      return message.reply('you must have the Manage Roles permission to use that command.');
     }
-		
+    if (message.member.roles.highest.comparePositionTo(message.guild.roles.resolve(timeoutRoleId)) <= 0) {
+      return message.reply(`you must have a role higher than <@&${timeoutRoleId}> to use that command.`);
+    }
+    if (!message.mentions.members.size) {
+      return message.reply('you must mention at least one member to DQ.');
+    }
+
     // strip @-mentions out of the args string because we just care about the duration and reason
-    args = args.split(' ').filter(e => !e.startsWith('@')).join(' ');
-    const [durationStr, reason] = args.split(' ', 2);
+    const [durationStr, ...reasonArr] = args.trim().split(/\s+/).filter(e => !MessageMentions.USERS_PATTERN.test(e));
+    const reason = reasonArr.join(' ');
 
     // make sure that the user only gives a valid shorthand duration specifier. moment doesn't check this properly still (see moment/moment#1805)
-    if (durationStr.match(durationRegex).length !== 2) {
+    const matches = durationStr.match(durationRegex)?.slice(1, 3);
+    if (matches?.length !== 2) {
       return message.reply(`invalid duration \`${durationStr}\`. valid units are \`y\`, \`M\`, \`w\`, \`d\`, \`h\`, \`m\`, \`s\`, and \`ms\`.`);
     }
 
-
-    //message.mentions.members.forEach(do_timeout(message.guild));
-		
-    const duration = moment.duration(...durationStr.match(durationRegex));
-    duration.timer({start: true}, doUnTimeout);
+    const duration = moment.duration(...matches);
+    duration.timer({start: true}, () => message.mentions.members.forEach(m => doUnTimeout(m)()));
     const dqEndTime = moment().add(duration).valueOf();
 
-    doTimeout(message.guild, {member: message.member, user: message.author}, message.mentions.members, duration, reason);
+    await doTimeout(message.guild, message.member, message.mentions.members, duration, reason);
 
-    await db().collection('dqs').updateMany({
-      _id: {
-        guild: message.guild.id,
-        channel: message.channel.id,
-        user: { $in: message.mentions.users.map(u => u.id) }
-      }
-    }, {
-      $set: { dqEndTime }
-    }, {
-      upsert: true
-    });
+    await db().collection<Dq>('dqs').bulkWrite(message.mentions.users.map<BulkWriteOperation<Dq>>(u => {
+      return {
+        updateOne: {
+          filter: {
+            _id: {
+              guild: message.guild.id,
+              user: u.id
+            }
+          },
+          update: {
+            $set: { dqEndTime }
+          },
+          upsert: true
+        }
+      };
+    }));
 
     // TODO: send message to channel
   }
