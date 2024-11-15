80d213df0940da76f4e1e184448a288009bdf18a
sy1vi3 <me@sylvie.fyi>
2024-11-14 20:32:32 -0700

    patch reverifier to not attempt to assign the nitro role

--- src/listeners/member-verification/member-add-reverifier.ts
+++ src/listeners/member-verification/member-add-reverifier.ts
@@ -2,6 +2,7 @@ import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener } from "@sapphire/framework";
 import type { GuildMember } from "discord.js";
 import { verifiedMembers } from "../../index.js";
+import reverifyIgnoreRoles from "../../config/reverify-ignore-roles.json" with { type: "json" };
 
 @ApplyOptions<Listener.Options>({ event: Events.GuildMemberAdd })
 export class GuildMemberAddListener extends Listener<
@@ -16,7 +17,7 @@ export class GuildMemberAddListener extends Listener<
       const reason = "Automatic reverification";
       await Promise.all([
         member.setNickname(verifiedMember.nickname, reason),
-        member.roles.add(verifiedMember.roles, reason),
+        member.roles.add(verifiedMember.roles.filter(item => !reverifyIgnoreRoles.includes(item)), reason),
       ]);
     }
   }
