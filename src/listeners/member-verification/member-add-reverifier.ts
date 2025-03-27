af2df40104ed4d9e5b2affe624357a0f070569dc
Sylvie <54775775+sy1vi3@users.noreply.github.com>
2025-03-27 16:14:33 -0400

    patch reverifier to not attempt to assign the nitro role (#22)
    ---------
    
    Co-authored-by: jtkiesel <jordantkiesel@gmail.com>
    Co-authored-by: sdy329 <sdy329@gmail.com>

--- src/listeners/member-verification/member-add-reverifier.ts
+++ src/listeners/member-verification/member-add-reverifier.ts
@@ -1,6 +1,6 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener } from "@sapphire/framework";
-import type { GuildMember } from "discord.js";
+import type { GuildMember, Role } from "discord.js";
 import { verifiedMembers } from "../../index.js";
 
 @ApplyOptions<Listener.Options>({ event: Events.GuildMemberAdd })
@@ -14,9 +14,19 @@ export class GuildMemberAddListener extends Listener<
     });
     if (verifiedMember) {
       const reason = "Automatic reverification";
+      const me = await member.guild.members.fetchMe();
+      const myHighestRole = me.roles.highest;
+      const assignableRoles = verifiedMember.roles
+        .map((role) => member.guild.roles.cache.get(role))
+        .filter(
+          (role): role is Role =>
+            role !== undefined &&
+            !role.managed &&
+            myHighestRole.comparePositionTo(role) > 0,
+        );
       await Promise.all([
         member.setNickname(verifiedMember.nickname, reason),
-        member.roles.add(verifiedMember.roles, reason),
+        member.roles.add(assignableRoles, reason),
       ]);
     }
   }
